86759ebeadbde8df4910e72f5fa141721dacee4f
Simon Anliker <anliksim@users.noreply.github.com>
2019-09-28 14:45:42 +0200

    Merge index and map page

--- public/resources/js/swipe-up.js
+++ public/resources/js/swipe-up.js
@@ -1,14 +1,16 @@
-var myElement = document.getElementById('vote');
+const voteElement = document.getElementById('vote');
+const mapElement = document.getElementById('map-container');
 
 // create a simple instance
 // by default, it only adds horizontal recognizers
-var mc = new Hammer(myElement);
+const mc = new Hammer(voteElement);
 
 // let the pan gesture support all directions.
 // this will block the vertical scrolling on a touch-device while on the element
-mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+mc.get('pan').set({direction: Hammer.DIRECTION_ALL});
 
 // listen to events...
-mc.on("panleft panright panup pandown tap press", function(ev) {
-    myElement.textContent = ev.type +" gesture detected.";
+mc.on("panleft panright panup pandown tap press", function (ev) {
+    voteElement.style.display = "none";
+    mapElement.style.display = "block";
 });

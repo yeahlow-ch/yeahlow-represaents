d01fb0cf03c48cf9af7c056ad67c3c36d316c1bc
Simon Anliker <anliksim@users.noreply.github.com>
2019-09-28 15:55:12 +0200

    Add event markers to map

---
+++ public/resources/js/axios-init.js
@@ -0,0 +1,5 @@
+const instance = axios.create({
+    baseURL: 'https://yeahlow.eu-de.mybluemix.net/api',
+    timeout: 5000,
+    withCredentials: false,
+});
--- public/resources/js/get-events.js
+++ public/resources/js/get-events.js
@@ -1,6 +1,6 @@
-function getVotes(callback) {
+function getEvents(callback) {
 // Make a< request for a user with a given ID
-    instance.get('/votes')
+    instance.get('/events')
         .then(function (response) {
             // handle success
             callback(response.data);
